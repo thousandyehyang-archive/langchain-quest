311a05e9ffdd4210ee6e3dd0f2210af30e1f3a07
1000hyehyang <ducogus12@gmail.com>
2025-02-10 15:34:44 +0900

    feat : 모델 사용

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,12 @@
 async function main() {
   async function hanldeCC(event) {
     event.preventDefault(); // Form의 기본 submit 막아줘야하고...
+
+    // 로딩을 추가했다가...
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner-border");
+    document.querySelector("#explore-text").appendChild(spinner);
+
     const url = "http://127.0.0.1:3000";
     const formData = new FormData(document.querySelector("#explore-form"));
     const text = formData.get("text");
@@ -13,9 +19,23 @@ async function main() {
         "Content-Type": "Application/json",
       },
     });
+
     const json = await response.json();
 
-    document.querySelector("#explore-text").textContent = JSON.stringify(json);
+    // 로딩을 추가했다가 종료시 없애는...
+    spinner.remove();
+
+    const { image, desc } = json;
+
+    const explore_text = document.querySelector("#explore-text");
+    explore_text.innerHTML = "";
+    const imageTag = document.createElement("img");
+    imageTag.classList.add("img-fluid");
+    imageTag.src = image;
+    const descTag = document.createElement("p");
+    descTag.textContent = desc;
+    explore_text.appendChild(imageTag);
+    explore_text.appendChild(descTag);
   }
 
   document.querySelector("#explore-form").addEventListener("submit", hanldeCC);
