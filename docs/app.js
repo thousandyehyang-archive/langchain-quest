271381ebeed3f59feca5849f331c038a87d2ddb0
1000hyehyang <ducogus12@gmail.com>
2025-02-10 14:34:25 +0900

    fix :

--- docs/app.js
+++ docs/app.js
@@ -1,13 +1,24 @@
 async function main() {
-  async function hanldeCC() {
-    document.querySelector("#recommend-text").textContent = await (
-      await fetch("http://127.0.0.1:3000")
-    ).text();
+  async function hanldeCC(event) {
+    event.preventDefault(); // Form의 기본 submit 막아줘야하고...
+    const url = "http://127.0.0.1:3000";
+    const formData = new FormData(document.querySelector("#explore-form"));
+    const text = formData.get("text");
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        text,
+      }),
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    });
+    const json = await response.json();
+
+    document.querySelector("#explore-text").textContent = JSON.stringify(json);
   }
 
-  document
-    .querySelector("#recommend-button")
-    .addEventListener("click", hanldeCC);
+  document.querySelector("#explore-form").addEventListener("submit", hanldeCC);
 }
 
 document.addEventListener("DOMContentLoaded", main);
